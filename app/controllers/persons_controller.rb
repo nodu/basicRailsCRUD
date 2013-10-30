182d409445d8fca16c0a5e7ed412c8f13de18518
Matt Nodurfth <mnodurft@gmail.com>
2013-10-30 10:50:02 +0800

    added json render and fleshed out controllers a bit

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -2,14 +2,24 @@ class PersonsController < ApplicationController # notice it's plural because we'
 	
 	# List all persons (show tabel of person in HTML)
 	def index
+		@persons = Person.all.entries #weird quirk of mongoid that returns a curser, not an array of all
+		#@ instance variable
+		#@@ class variables
+
+		render json: @persons #skips the view, and just renders out the json
+
 	end
 
 	# Show detail for a person with ID = params[:id] (in HTML)
 	def show
+		@person = Person.find_by(id: params[:id])
+		render json: @person #skips the view, and just renders out the json
 	end
 
 	# Form for adding a new person (in HTML)
 	def new
+		@person = Person.new
+		render json: @person #skips the view, and just renders out the json
 	end
 
 	# POST to this to crea a new person, then redirect to show
@@ -19,6 +29,8 @@ class PersonsController < ApplicationController # notice it's plural because we'
 
 	#Form for updating a person with ID = params[:id] (in HTML)
 	def edit
+		@person = Person.find_by(id: params[:id])
+		render json: @person #skips the view, and just renders out the json
 	end
 
 	# PUT or PATCH to this to update person with ID = params[:id] , then redirect to show
@@ -30,7 +42,7 @@ class PersonsController < ApplicationController # notice it's plural because we'
 	# Destroy the person with ID = params[:id]
 	# then redirect to index to list remaining persons
 	def destroy
-		
+
 		redirect_to 'index'
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,26 @@
 CrapCrud::Application.routes.draw do
+  resources :persons  #does the exact same as all below
+  # if using a GET it will usually return a HTML, nulipotent, doens't change!
+  # if POST PUT PATCH DELETE, it will usually redirect to a get; itempotent makes changes! 
 
-  # root 'welcome#index'
-  get 'persons' => 'persons#index'  # list all person # gets are nulipotent, doesn't change anything, safe, only gets
-  get 'persons/new' => 'persons#new' #form to add a new person
- # new needs to be before :id, it thinks new is the id!
-  get 'persons/:id' =>  'persons#show' #name of collection passing in an id  #show one person
+  ##### FOR JSON API
+  # resources :persons, except: [:new, :edit]
+
+
+ root 'persons#index'
+ #  get 'persons' => 'persons#index'  # list all person # gets are nulipotent, doesn't change anything, safe, only gets
+ #  get 'persons/new' => 'persons#new' #form to add a new person
+ # # new needs to be before :id, it thinks new is the id!
+ #  get 'persons/:id' =>  'persons#show' #name of collection passing in an id  #show one person
 
   
-  get 'persons/:id/edit' => 'persons#edit' #form to edit person
+ #  get 'persons/:id/edit' => 'persons#edit' #form to edit person
 
-  post 'persons' => 'persons#create' #create a new person, ust like p = Person.new?
-  put 'persons/:id' => 'persons#update' #update a person
-  patch 'persons/:id' => 'persons#update' #update an attribute of a person
-  delete 'persons/:id' => 'persons#destroy' #delete a person, delete from a db, objects get destroyed
+ #  post 'persons' => 'persons#create' #create a new person, ust like p = Person.new?
+ #  put 'persons/:id' => 'persons#update' #update a person
+ #  patch 'persons/:id' => 'persons#update' #update an attribute of a person
+ #  delete 'persons/:id' => 'persons#destroy' #delete a person, delete from a db, objects get destroyed
 end
+
+
+# the prefix in rake routes are the url paths! root_url, persons_url
