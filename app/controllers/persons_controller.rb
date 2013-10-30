c53b3383d6df89de7d0d5a84a809f51547c4bc7f
Matt Nodurfth <mnodurft@gmail.com>
2013-10-30 17:35:19 +0800

    Working basic CRUD app, view all, view one, new person, edit person, delete person

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -24,28 +24,38 @@ class PersonsController < ApplicationController # notice it's plural because we'
 
 	# POST to this to crea a new person, then redirect to show
 	def create
-		@person = Person.new(person_params)
-		@person.save
-		redirect_to 'index'
+		@person = Person.create(person_params)
+		redirect_to person_url(@person)
 	end
 
 	#Form for updating a person with ID = params[:id] (in HTML)
 	def edit
 		@person = Person.find_by(id: params[:id])
+		# @person.save
 		# render json: @person #skips the view, and just renders out the json
 	end
 
 	# PUT or PATCH to this to update person with ID = params[:id] , then redirect to show
 	def update
+		@person = Person.find_by(id: params[:id])
+		@person.update_attributes(person_params)
 
-		redirect_to 'index'
+		# Same as below!
+		# @person.name = params[:person][:name]
+		# @person.age = params[:person][:age]
+		# @person.is_a_clown = params[:person][:is_a_clown]
+		# @person.save
+
+		# redirect_to 'index'
+		redirect_to person_url(@person)  # the prefix from rake routes, could also use path..
 	end
 	
 	# Destroy the person with ID = params[:id]
 	# then redirect to index to list remaining persons
 	def destroy
-
-		redirect_to 'index'
+		@person = Person.find_by(id: params[:id])
+		@person.destroy
+		redirect_to persons_url
 	end
 	
 	def person_params
