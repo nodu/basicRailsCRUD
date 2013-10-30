a67fd3e3fd2eea719ce4d1b55064178dc67e5513
Matt Nodurfth <mnodurft@gmail.com>
2013-10-30 11:22:10 +0800

    added views, changed person/people to person/persons in inflections.rb, updated controller to provide functionality

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -6,31 +6,33 @@ class PersonsController < ApplicationController # notice it's plural because we'
 		#@ instance variable
 		#@@ class variables
 
-		render json: @persons #skips the view, and just renders out the json
+		#render json: @persons #skips the view, and just renders out the json
 
 	end
 
 	# Show detail for a person with ID = params[:id] (in HTML)
 	def show
 		@person = Person.find_by(id: params[:id])
-		render json: @person #skips the view, and just renders out the json
+		# render json: @person #skips the view, and just renders out the json
 	end
 
 	# Form for adding a new person (in HTML)
 	def new
 		@person = Person.new
-		render json: @person #skips the view, and just renders out the json
+		# render json: @person #skips the view, and just renders out the json
 	end
 
 	# POST to this to crea a new person, then redirect to show
 	def create
+		@person = Person.new(person_params)
+		@person.save
 		redirect_to 'index'
 	end
 
 	#Form for updating a person with ID = params[:id] (in HTML)
 	def edit
 		@person = Person.find_by(id: params[:id])
-		render json: @person #skips the view, and just renders out the json
+		# render json: @person #skips the view, and just renders out the json
 	end
 
 	# PUT or PATCH to this to update person with ID = params[:id] , then redirect to show
@@ -44,6 +46,12 @@ class PersonsController < ApplicationController # notice it's plural because we'
 	def destroy
 
 		redirect_to 'index'
+	end
+	
+	def person_params
+		params.require(:person).permit(
+			:name, :age, :is_a_clown
+			)
 	end
 
 
